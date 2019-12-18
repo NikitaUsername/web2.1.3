1e494ee3f69df73ca29ed1aa23f191e5498f7080
Nikita <nikita.kalyaev@gmail.com>
2019-12-18 19:28:43 +0300

    delete some trash

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,7 @@ var gulp = require('gulp'),
 	path = require('path');
 
 
-gulp.task('nunjucks', function() {
-	return gulp.src('./src/njk/index.njk')
-		.pipe(njkRender())
-		.pipe(prettify({
-			indent_size : 4 
-        }))
-		.pipe(gulp.dest('./src/'));
-});
+
 
 
 gulp.task('sass', function() {
@@ -36,9 +29,7 @@ gulp.task('browser-sync', gulp.series('sass'), function() {
 
 gulp.task('watch', function () {
     gulp.watch('./src/sass/*.sass', gulp.series('sass'));
-	gulp.watch('./**/*.njk', gulp.series('nunjucks'));
     gulp.watch('./src/css/*.css').on("change", browserSync.reload);
-    gulp.watch('./src/index.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('sass', 'nunjucks', 'watch','browser-sync'));
+gulp.task('default', gulp.series('sass', 'watch','browser-sync'));
